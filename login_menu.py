f0ac77d8d218c5d6718cad76fc16bab6a282b961
Metehan PR <hazardpekacar@gmail.com>
2024-11-28 19:35:14 +0300

    project

---
+++ login_menu.py
@@ -0,0 +1,29 @@
+import pyodbc
+
+class LoginInterface:
+    def __init__(self):
+        try:
+            # Bağlantıyı oluştur
+            self.connection = pyodbc.connect(
+                'DRIVER={SQL Server};' +
+                'Server=DESKTOP-T5KKBFV;' +
+                'Database=master;' +
+                'Trusted_Connection=True'
+            )
+            self.cursor=self.connection.cursor()
+    def control_check(self,username,password):
+        try:
+            sql="SELECT*FROM USERS WHERE username=? AND password=?"
+            self.cursor.execute(sql,(username,password))
+            user=self.cursor.fetchone()
+            if user:
+                return{'flag':True,'role':'user.role','username':'user.username'}
+            else:
+                print('Login failed!')
+                return {'flag':False }
+            
+        except pyodbc.Error as ex:
+            print('Connection Failed', ex)
+            
+  
+
